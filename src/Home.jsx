9bf97e2cbb6705cd8350269022e0c11e2d451f5d
SteoXX <stefano.garda2004@gmail.com>
2023-12-11 21:27:20 +0100

    Started working on the home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,72 +1,78 @@
-import React from "react";
-//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import Drawer from "@material-ui/core/Drawer";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import React, { useState } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ViewHeadlineIcon from "@mui/icons-material/ViewHeadline";
+import Box from "@mui/material/Box";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawer: {
-    width: "240px",
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: "240px",
-  },
-  drawerContainer: {
-    overflow: "auto",
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
+import { withTheme } from "styled-components";
+import { darken } from "polished";
 
-export default function Home() {
-  const classes = useStyles();
+function Home({ theme }) {
+  const [open, setOpen] = useState(false);
+
+  const handleShowHideDrawer = () => {
+    setOpen(!open);
+  };
 
   return (
-    <div className={classes.root}>
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <IconButton color="inherit">
-            <AccountCircle />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-            {["Section 1", "Section 2", "Section 3"].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-        </div>
-      </Drawer>
-      <main className={classes.content}>
-        <Toolbar />
-        {/* Here you can map through your products and display them */}
-      </main>
+    <div>
+      <div sx={{ display: "flex" }}>
+        <AppBar
+          position="fixed"
+          sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+          }}
+        >
+          <Toolbar sx={{ bgcolor: darken(0.08, theme?.bgcolor) }}>
+            {/* Icon Button for show/hide Drawer */}
+            <IconButton color="inherit" onClick={handleShowHideDrawer}>
+              <ViewHeadlineIcon />
+            </IconButton>
+
+            <Box sx={{ flexGrow: 1 }} />
+
+            {/* Icon Button for account managment */}
+            <IconButton color="inherit">
+              <AccountCircle />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          open={open}
+          variant="temporary"
+          sx={{
+            width: "240px",
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: "240px",
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Toolbar />
+          <div sx={{ overflow: "auto" }}>
+            <List>
+              {["Section 1", "Section 2", "Section 3"].map((text, index) => (
+                <ListItem button key={text}>
+                  <ListItemText primary={text} />
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        </Drawer>
+        <main sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          {/* Here you can map through your products and display them */}
+        </main>
+      </div>
     </div>
   );
 }
+
+export default withTheme(Home);
